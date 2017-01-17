d6f53a706438d59bd37d5b432259b375e968c5e0
Dylan <dylan4047@gmail.com>
2017-01-17 10:56:04 +0100

    ajout des function getter et setter pour les propriéter wheels kilometrage abs et doors

--- lib/Car.php
+++ lib/Car.php
@@ -16,11 +16,35 @@ class Car {
 	public function getWheels() {
 		return $this->wheels;
 	}
-
 	/**
 	 * Retrourne une chaîne de caractère
 	 * 
 	 */
+	public function setWheels($value){
+		$this->wheels = $value;
+	}
+
+	public function getKilometrage(){
+		return $this->$kilometrage;
+
+	}
+	public function setKilometrage($value){
+		$this->$kilometrage = $value;
+	}
+
+	public function getAbs(){
+		return $this->$abs;
+	}
+	public function setAbs($value){
+		$this->$abs = $value;
+	}
+
+	public function getDoors(){
+		return $this->$doors;
+	}
+	public function setDoors($value){
+		$this->$doors = $value;
+	}
 
 	public function getManufacturer() {
 		return $this->manufacturer;
@@ -29,18 +53,15 @@ class Car {
 	public function setManufacturer($name) {
 		$this->manufacturer = $name;
 	}
-
 	/**
 	 *	Retourne un booléen sur la disponibilité
 	 *	des airbags
 	 * 
 	 * @return  {boolean} Are airbag available 
 	 */
-
 	public function getAirbag() {
 		return $this->airbag;
 	}
-
 	/**
 	 * Définition de la disponibilité des airbags
 	 * 
